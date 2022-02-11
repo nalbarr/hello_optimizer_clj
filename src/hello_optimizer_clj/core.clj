8e0a6f7ca888084ecd2a40b386d555e0b7e488eb
nalbarr <nalbarracin@gmail.com>
2022-02-10 20:44:11 -0600

    NA. Fix sqrt-iter() syntax errors where if, then syntax and fuctional evaluation.

--- src/hello_optimizer_clj/core.clj
+++ src/hello_optimizer_clj/core.clj
@@ -2,44 +2,77 @@
   (:gen-class)
   (:require [clojure.math.numeric-tower :as math]))
 
-(defn bar [x]  
+(defn bar
+  [x]  
   (+ x 1))
 
-(defn square [x]
+(defn square
+  [x]
   (* x x))
 
-(defn average [x y]
+(defn average
+  [x y]
   (/ (+ x y) 2.0))
 
-(defn improve [guess x]
+(defn improve
+  [guess x]
   (average guess (/ x guess)))
 
-(defn good-enough [guess x]
+(defn good-enough
+  [guess x]
   (< (math/abs (- (square guess) x)) 0.001))
 
-(defn new-if [predicate then-clause else-clause]
-  (cond
-    (boolean? predicate) (then-clause)
-    :else (else-clause)))
+(defn new-if
+  [predicate then-clause else-clause]
+  (if predicate 
+    then-clause 
+    else-clause))
 
-(defn sqrt-iter [guess x]
+(defn sqrt-iter
+  [guess x]
   (new-if (good-enough guess x)
       guess
-      (sqrt-iter (improve guess x)
-                 x)))
+      (sqrt-iter (improve guess x) x)))
 
-(defn square-root [x]
-  (sqrt-iter 1.0 x))  
+(defn square-root
+  [x]
+  (sqrt-iter 1.0 x))
+
+(defn then-clause
+  [x]
+  (print "then: " + x)
+  x)  
+
+(defn else-clause
+  [y]
+  (print "else: " + y)
+  y)  
+
 
 (defn -main
   "Simple optimization."
   [& args]
-  (println "bar: "(bar 2))
+
   (println "square: " (square 2))
+
   (println "average: " (average 1 3))
-  (println "improve: " (improve 5. 3.))
-  (println "good-enough: " (good-enough 5. 3.)))
+  
+  (println "improve: " (improve 4. 3.))
+
+  (println "good-enough: " (good-enough 4. 3.))
+
+  (println "new-if: "
+    (new-if 
+      (good-enough 4. 3.);      false
+      (then-clause 123)
+      (else-clause 345 )))
+
+  (println "new-if: "
+    (new-if 
+      (good-enough 4. 2.00001);   true
+      (then-clause 123)
+      (else-clause 345 )))
+
   ; TODO: NA. Finish me.
-  ; (println "new-if: ") (new-if (good-enough 5. 3.) (print true) (print false))
-  ; (println "new-if: ") (new-if (good-enough 5. 5.00001) (print true) (print true)))  
-  ; (println "square-root: " (square-root 3)))
+  ; - issue with sqrt-iter
+  ;(println "square-root: " (square-root 3)))
--- test/hello_optimizer_clj/core_test.clj
+++ test/hello_optimizer_clj/core_test.clj
@@ -2,6 +2,44 @@
   (:require [clojure.test :refer :all]
             [hello-optimizer-clj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest square-2
+  (testing "square of 2 is 4."
+    (is (= 4 ) (square 2))))
+
+(deftest average-1-and-3
+  (testing "average of 1 and 3 is 2."
+    (is (= 2 ) (average 1 3))))
+
+(deftest improve-5-and-3
+  (testing "improve-guess 5 and 3 is 2.8"
+    (is (= 2.8 ) (improve 5. 3.))))
+
+(deftest good-enough-5-and-3
+  (testing "good-enough 5 and 3 is false"
+    (is (= false ) (good-enough 5. 3.))))
+
+(deftest good-enough-5-and-5_00001
+  (testing "good-enough 5 and 5.00001 is false"
+    (is (= true ) (good-enough 5. 3.))))
+
+(defn predicate-true
+  [x]
+  (= 1 1)); always true
+
+(defn predicate-false
+  [x]
+  (= 2 1)); always false
+  
+(deftest new-if-predicate-true
+  (testing "new-if is true"
+    (is (= 123)
+      (new-if (predicate-true 1)
+        (then-clause 123) 
+        (else-clause 345)))))
+
+(deftest new-if-predicate-false
+  (testing "new-if is false"
+    (is (= 345)
+      (new-if (predicate-false 1)
+        (then-clause 123) 
+        (else-clause 345)))))          
